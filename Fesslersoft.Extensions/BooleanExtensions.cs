187af51453c0d9a80fc5b78c90f4c5c067861d47
Belial09 <realliferulor@gmx.de>
2015-02-17 21:40:18 +0100

    Cleanup

---
+++ Fesslersoft.Extensions/BooleanExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fesslersoft.Extensions
+{
+    public static class BooleanExtensions
+    {
+        /// <summary>
+        ///     Converts the value of this instance to its equivalent JSON string representation (either "true" or "false").
+        /// </summary>
+        /// <param name="boolean"></param>
+        /// <returns>string</returns>
+        public static string ToJson(this bool boolean)
+        {
+            return boolean ? "true" : "false";
+        }
+    }
+}
---
+++ Fesslersoft.Extensions/DictionaryExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fesslersoft.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        /// <summary>
+        ///     If the dictionary does not contain an item keyed with <paramref name="key" />, the
+        ///     value returned by the <paramref name="lookup" /> method is added to the dictionary --
+        ///     even if it is null
+        /// </summary>
+        public static void AddIfNotExist<TOne, TTwo>(this Dictionary<TOne, TTwo> dictionary, TOne key, Func<TOne, TTwo> lookup)
+        {
+            if (!dictionary.ContainsKey(key))
+            {
+                dictionary.Add(key, lookup(key));
+            }
+        }
+    }
+}
