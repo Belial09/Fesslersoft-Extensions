6493afe5cc63feddaa9d97fdd0b586f46380d2aa
Belial09 <realliferulor@gmx.de>
2015-02-17 20:34:52 +0100

    Added IsUpperCase Extension from http://codesnippets.fesslersoft.de/check-if-a-character-is-uppercase/

--- Fesslersoft.Extensions/CharacterExtensions.cs
+++ Fesslersoft.Extensions/CharacterExtensions.cs
@@ -14,5 +14,15 @@ namespace Fesslersoft.Extensions
             var checkStr = inputChar.ToString(CultureInfo.InvariantCulture).ToUpper();
             return (checkStr == "A" || checkStr == "E" || checkStr == "I" || checkStr == "O" || checkStr == "U");
         }
+
+        /// <summary>
+        ///     Checks whether the character is an uppercase Character or not
+        /// </summary>
+        /// <param name="inputChar"></param>
+        /// <returns>True or False depending on the character</returns>
+        public static bool IsUpperCase(this char inputChar)
+        {
+            return inputChar >= 'A' && inputChar <= 'Z';
+        }
     }
 }
