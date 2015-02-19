eee003dc0ed963b1e6f59f88f5ca0ee9d7e2b5d5
Belial09 <realliferulor@gmx.de>
2015-02-19 20:24:12 +0100

    Added IsPowerOfTwo Extension method -> http://codesnippets.fesslersoft.de/check-if-a-number-is-a-power-of-2/

---
+++ Fesslersoft.Extensions/NumericExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fesslersoft.Extensions
+{
+    public static class NumericExtensions
+    {
+        public static bool IsPowerOfTwo(this ulong input)
+        {
+            return (input != 0) && ((input & (input - 1)) == 0);
+        }
+    }
+}
