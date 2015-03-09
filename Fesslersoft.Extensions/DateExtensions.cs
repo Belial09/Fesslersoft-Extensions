1fa68c3922b0170409ed69e3b21dd1398e83ed46
Belial09 <realliferulor@gmx.de>
2015-03-09 10:25:35 +0100

    Added Hours Difference Extension from http://codesnippets.fesslersoft.de/get-hours-difference-of-2-dates/

---
+++ Fesslersoft.Extensions/DateExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fesslersoft.Extensions
+{
+    public static class DateExtensions
+    {
+        /// <summary>
+        /// Returns the Hour Difference of two Datetimes as Double Value.
+        /// </summary>
+        /// <param name="dateOne">The original date.</param>
+        /// <param name="dateTwo">The Datei to be Checked against the Original Date.</param>
+        /// <returns></returns>
+        public static Double DifferenceInHours(this DateTime dateOne, DateTime dateTwo)
+        {
+            return (dateOne - dateTwo).TotalHours;
+        }
+    }
+}
